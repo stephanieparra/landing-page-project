d1ac352c1b642b9ff9b25d148059b2dd7748657c
stephanieparra <stephanieparrastudio@gmail.com>
2023-05-16 20:09:06 -0700

    Added p5.js mouse follower

--- src/sketch.js
+++ src/sketch.js
@@ -1,18 +1,27 @@
 let canvas;
 let pig;
+let mouseFollower;
+
+function preload() {
+  pig = loadImage("images/pd (2).png");
+}
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("canvas-container");
   canvas.position(0, 0);
-  pig = loadImage("images/pd (2).png");
   canvas.style("z-index", "-1");
+
+  mouseFollower = createImg("images/pd (2).png");
+  mouseFollower.size(65, 65);
+  mouseFollower.position(0, 0);
+  mouseFollower.style("pointer-events", "none");
+  mouseFollower.style("z-index", "9999");
+  mouseFollower.style("border", "none");
 }
 
 function draw() {
-  background(175);
-  clear();
-  image(pig, mouseX, mouseY, 65, 65);
+  mouseFollower.position(mouseX, mouseY);
 }
 
 function windowResized() {
